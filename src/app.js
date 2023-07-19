6d16963c4bdd1000fb829656365542e97a53d072
phuti28 <139022343+phuti28@users.noreply.github.com>
2023-07-19 09:42:22 +0200

    updated the Javascript

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
     let date = new Date(timestamp);
     let hours = date.getHours();
     if (hours < 10) {
-        hours = 0${hours};
+        hours = `0${hours}`;
     }
     let minutes = date.getMinutes();
     if (minutes < 10) {
-        minutes = 0${minutes};
+        minutes = `0${minutes}`;
     }
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
@@ -21,16 +21,17 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
-    temperatureElement.innerHTML = Mathe.round(response.data.main.temp);
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.description;
+    descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-
+let city = "New York";
 let apiKey = "50c2acd53349fabd54f52b93c8650d37";
-let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=${apiKey}";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
