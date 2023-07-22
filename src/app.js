2f1df1839d779a74f539d376dc70eb686356ca12
phuti28 <139022343+phuti28@users.noreply.github.com>
2023-07-22 18:55:23 +0200

    updated the image Url

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ function displayForecast(response) {
         <div class = "col-2">
         <div class = "weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-        src = "https://openweathermap.org/img/wn/${forecastDay.weather.[0].icon}@2x.png"
+        src = "https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt = ""
         width = "42"
         />
